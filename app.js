14200e030abc44d42bcfbf8b4f94cba3f5f6e081
LuizAuGFer <luiz.augusto9922@outlook.com>
2020-11-29 10:24:38 -0300

    Configurado banco de dados – realizado cadastro e seleção de comentários

--- app.js
+++ app.js
@@ -3,15 +3,23 @@ const bodyParser=require('body-parser');
 const mysql=require('mysql');
 const handlebars=require ('express-handlebars');
 const app=express();
-
 const urlencodeParser = bodyParser.urlencoded({extended:false});
+
+//Mysql Connect
+const sql=mysql.createConnection({
+    host: 'localhost',
+    user: 'luiz',
+    password: '9922',
+    port: '3306'
+});
+sql.query("use nodejs");
 app.use(bodyParser.json());
+
 //Start Server
 app.listen(3000, function(req, res){
     console.log('Servidor rodando');
 });
 
-app.use(bodyParser.json());
 //Template Engine
 app.engine("handlebars",handlebars({defaultLayout:'main'}));
 app.set('view engine', 'handlebars');
@@ -21,6 +29,18 @@ app.use('/js', express.static('js'));
 
 //Routes and Templates
 app.get('/', function(req, res){
-    res.render('index');
+    sql.query("SELECT * FROM comment", function(err, results, fields){
+        res.render('index', {data: results});
+        
+    });
+   
+});
+app.post('/insert', urlencodeParser, function(req, res ){
+    sql.query("INSERT INTO comment  (dsc_comment) VALUES ('"+[req.body.comment]+"')");
+    res.redirect('/');  
+});
+
+app.post('/speat', urlencodeParser, function(req, res){
+    
 });
 
